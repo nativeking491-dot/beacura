57de23f97d1ade47cda848292d774efdf6ececf3
danny5564 <rajivkumat1987@gmail.com>
2026-02-28 01:30:49 +0530

    chore: setup capacitor and github actions for android apk generation

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.beacura.app',
+  appName: 'beacura',
+  webDir: 'dist'
+};
+
+export default config;
