89b8296e2cd8a71e9cbb8c04b0120cc22fada9e5
danny5564 <rajivkumat1987@gmail.com>
2026-04-18 00:38:28 +0530

    chore: remove apk build files and configurations

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.beacura.app',
-  appName: 'beacura',
-  webDir: 'dist'
-};
-
-export default config;
